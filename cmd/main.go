a6bf122c69da468effcd542942861bf14da89334
bivol <vbivol394@gmail.com>
2026-02-21 13:01:02 +0200

    populat backend pool de servere :

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"loadBal/internal/loader"
 	"loadBal/internal/server"
 )
 
 func main() {
+	loader.AddBackEnd()
 	Server_data := server.Servers
 	for _, data := range Server_data {
 		go server.Server(data.Id, data.Port)
--- internal/loader/pool.go
+++ internal/loader/pool.go
@@ -1,1 +1,21 @@
 package loader
+
+import (
+	"loadBal/internal/server"
+)
+
+type Backend struct {
+	Id    int
+	Url   string
+	Alive bool
+}
+
+var Backends []Backend
+
+func AddBackEnd() {
+	serversData := server.Servers
+	for _, data := range serversData {
+		Backends = append(Backends, Backend{Id: data.Id, Url: "http://localhost" + data.Port, Alive: data.Alive})
+	}
+
+}
