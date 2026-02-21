09b1bdb7f72941305ce419ccf93f1d8a62267859
bivol <vbivol394@gmail.com>
2026-02-21 13:11:20 +0200

    am Selectare la backend

--- internal/loader/pool.go
+++ internal/loader/pool.go
@@ -10,6 +10,7 @@ type Backend struct {
 	Alive bool
 }
 
+var currentIndex int = 0
 var Backends []Backend
 
 func AddBackEnd() {
@@ -18,4 +19,16 @@ func AddBackEnd() {
 		Backends = append(Backends, Backend{Id: data.Id, Url: "http://localhost" + data.Port, Alive: data.Alive})
 	}
 
+}
+
+func SelectBackEnd() Backend {
+	var AliveBackends []Backend
+	for _, data := range Backends {
+		if data.Alive == true {
+			AliveBackends = append(AliveBackends, data)
+		}
+	}
+	backend := AliveBackends[currentIndex%len(AliveBackends)]
+	currentIndex++
+	return backend
 }
